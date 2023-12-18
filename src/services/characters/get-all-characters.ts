ce48de55d100890c78473025cf9fa19198f54cc1
Wilmer Alegria <wilmercorrea53@gmail.com>
2023-12-17 22:15:15 -0500

    refactor: :recycle: Se agrega tipado al metodo que optiene los personajes.

--- src/services/characters/get-all-characters.ts
+++ src/services/characters/get-all-characters.ts
@@ -1,6 +1,7 @@
+import { Icharacters } from '../../interfaces/data.interface';
 import { baseUrl } from '../base';
 
-export const getAllCharacters = async () => {
+export const getAllCharacters = async ():Promise<Icharacters> => {
     const response = await fetch(`${baseUrl}character`);
     const allCharactersData = await response.json();
     return allCharactersData;
