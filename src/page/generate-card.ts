f265ea73d42011496b24ee08d5c61f557ed20951
Wilmer Alegria <wilmercorrea53@gmail.com>
2023-12-19 23:30:35 -0500

    feat: :sparkles: Se agrega status a la card para mostar el estado actual del personaje.

--- src/page/generate-card.ts
+++ src/page/generate-card.ts
@@ -1,5 +1,8 @@
 
-export const generateCard = ( name:string, urlImage:string, specie:string ):string => {
+export const generateCard = (name: string, urlImage: string, specie: string, status: string): string => {
+
+    const statusClass = generateStatus(status)
+
     return `
         <div class="card">
             <div class="card-image">
@@ -9,12 +12,33 @@ export const generateCard = ( name:string, urlImage:string, specie:string ):stri
             </div>
             <div class="card-content">
             <div class="media">
-                <div class="media-content">
-                <p class="title is-4">${name}</p>
-                <p class="subtitle is-6">${specie}</p>
+            <div class="media-content">
+                    <div class="status ${statusClass}">${status}</div>
+                    <p class="title is-4">${name}</p>
+                    <p class="subtitle is-6">${specie}</p>
                 </div>
             </div>
             </div>
         </div>
     `
+};
+
+const generateStatus = (status: string) => {
+
+    let statusClass;
+
+    switch (status) {
+        case 'Alive':
+            statusClass = 'alive';
+            break;
+        case 'Dead':
+            statusClass = 'dead';
+            break;
+        default:
+            statusClass = 'unknown';
+            break;
+    }
+
+    return statusClass
+
 }
