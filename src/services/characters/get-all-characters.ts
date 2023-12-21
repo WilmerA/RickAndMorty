55054b5cb0cf0bfa7c140ec2442410e8fc3223d5
Wilmer Alegria <wilmercorrea53@gmail.com>
2023-12-20 22:23:11 -0500

    refactor: :recycle: Se refactoriza metodo que obtiene los personajes, agregando el parametro de página.

--- src/services/characters/get-all-characters.ts
+++ src/services/characters/get-all-characters.ts
@@ -1,8 +1,8 @@
 import { Icharacters } from '../../interfaces/data.interface';
 import { baseUrl } from '../base';
 
-export const getAllCharacters = async ():Promise<Icharacters> => {
-    const response = await fetch(`${baseUrl}character`);
+export const getAllCharacters = async ( page:number = 1 ):Promise<Icharacters> => {
+    const response = await fetch(`${baseUrl}character?page=${page}`);
     const allCharactersData = await response.json();
     return allCharactersData;
 }
