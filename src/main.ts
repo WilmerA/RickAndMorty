06ce3b866f7e92b332a70304700c442e14c0affc
Wilmer Alegria <wilmercorrea53@gmail.com>
2023-12-25 20:51:59 -0500

    feat(Behavior): :sparkles: Se agrega evento listenner a los botones de siguiente y anterior.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { loadCards } from './page/cards/load-cards';
+import { setPaginator } from './page/paginator/set-paginator';
 import { getCharactersPerPages } from './services/characters/get-characters-per-pages';
 import './style.css'
 
@@ -6,6 +7,7 @@ import './style.css'
 const main = async () => {
   const { results:characters} = await getCharactersPerPages();
   loadCards( characters );
+  setPaginator()
 }
 
 main()
--- src/page/paginator/set-paginator.ts
+++ src/page/paginator/set-paginator.ts
@@ -1,15 +1,57 @@
 import { createInitialPaginator } from "./generate-paginator";
 
-let currentPage:number = 1;
-
 const paginator = document.querySelector('.pagination');
 console.log('pagination', paginator);
 
 
 export const setPaginator = ( page:number = 1 ) => {
-    
-    const itemsPaginator:HTMLElement = createInitialPaginator(currentPage);
+    const ulItem = document.querySelector('.pagination-list');
+    if( ulItem ){
+        ulItem.remove();
+    }
+    const itemsPaginator:HTMLElement = createInitialPaginator(page);
     paginator?.appendChild(itemsPaginator);
-
+    console.log(page);
+    
+    return page;
 }
 
+
+
+const _listtenerButtonNext = () => {
+
+    const buttonNext = document.querySelector('.pagination-next');
+    
+    buttonNext?.addEventListener( 'click', () => {
+        const currentElement = document.querySelector('.is-current');
+        const nextPage = Number(currentElement?.textContent) + 1;
+
+        setPaginator(nextPage);
+        
+
+    })
+
+};
+
+const _listtenerButtonPrev = () => {
+
+    const buttonPrev = document.querySelector('.pagination-previous');
+    
+    buttonPrev?.addEventListener( 'click', () => {
+        const currentElement = document.querySelector('.is-current');
+        const prevPage = Number(currentElement?.textContent) - 1;
+        setPaginator(prevPage);
+
+    } )
+
+};
+
+const _setListeners = () => {
+
+    _listtenerButtonNext();
+    _listtenerButtonPrev();
+
+
+};
+
+_setListeners();
